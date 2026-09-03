f6bc05d0fcf1aab562d11dfb56d94b26d0ab0ff0
Purlieu Studios <purlieustudios.develop@gmail.com>
2026-09-02 22:41:55 -0500

    feat: Add Inventory to gateway

--- src/backend/OrderForge.Gateway/Program.cs
+++ src/backend/OrderForge.Gateway/Program.cs
@@ -7,6 +7,11 @@ var ordersBaseUrl = builder.Configuration["Services:Orders:BaseUrl"]
 
 builder.Services.AddHttpClient("Orders", client => client.BaseAddress = new Uri(ordersBaseUrl));
 
+var inventoryBaseUrl = builder.Configuration["Services:Inventory:BaseUrl"]
+    ?? throw new InvalidOperationException(
+        "Configuration 'Services:Inventory:BaseUrl' is not configured.");
+
+builder.Services.AddHttpClient("Inventory", client => client.BaseAddress = new Uri(inventoryBaseUrl));
 
 var app = builder.Build();
 app.MapControllers();
